3e67650cc92a44981d58b0f643e7483f2211e60f
thiru-2004-harshu <thiru556678@gmail.com>
2024-10-15 10:56:16 +0530

    message

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <h1>Creating to all developer</h1>
+  </React.StrictMode>
+);
+
+
